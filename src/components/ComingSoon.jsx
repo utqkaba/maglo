181ed7b0db284ee269a5ec6365871acfec6fe1b1
utqkaba <utqkaba@gmail.com>
2025-11-25 00:28:20 +0300

    image update

--- src/components/ComingSoon.jsx
+++ src/components/ComingSoon.jsx
@@ -2,9 +2,9 @@ export default function ComingSoon({ title }) {
   return (
     <div className="flex flex-col items-center justify-center h-[60vh] text-center">
       <img
-        src="../public/comingSoon.gif"
+        src="../public/comingSoon.jpg"
         alt="Coming Soon"
-        className="w-auto h-28 mb-6 mr-12"
+        className="w-auto h-80"
       />
 
       <h1 className="text-3xl font-bold text-gray-800 mb-4">{title} Page</h1>
